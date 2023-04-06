28aee1a4e3e2de23a666c4416c36fece356b2805
Calum Couch <ccouch@lcs.on.ca>
2023-04-06 09:54:39 -0400

    Initial Commit

---
+++ TO-DO list/TO_DO_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  TO_DO_listApp.swift
+//  TO-DO list
+//
+//  Created by calum couch on 2023-04-06.
+//
+
+import SwiftUI
+
+@main
+struct TO_DO_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
